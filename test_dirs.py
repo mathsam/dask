0fc0f6aaf7c75858d76247ce91e0e092357b2527
Junyi Chai <phychai.junyi@gmail.com>
2017-01-08 15:58:02 -0500

    gather within tasks before return

--- test_dirs.py
+++ test_dirs.py
@@ -7,6 +7,7 @@ client.restart()
 from funcs import create_dirs, get_dirs, add_flag
 
 future = client.map(create_dirs, range(100))
-flags = client.submit(get_dirs, future)
+flags = []
+flags.append(client.submit(get_dirs, future))
 client.gather(flags)
 print(flags)
